0fb8b11b4632b9d00f3517c09cf7c4fdb3ba7b29
shiokazexf <2240373075@qq.com>
2023-03-18 04:55:50 +0800

    update

--- pkg/server/handler.py
+++ pkg/server/handler.py
@@ -4,6 +4,7 @@ import openai
 from fastapi import FastAPI
 
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 app.add_middleware(
@@ -15,18 +16,19 @@ app.add_middleware(
 )
 
 
+class GBTForwardReq(BaseModel):
+    message: list
+
+
 @app.post("/chat")
-def chat(message):
+def chat(req: GBTForwardReq):
     print("msg received")
     rsp = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=message
+        messages=req.message
     )
     try:
         print("proxy rsp" + json.dumps(rsp))
     except Exception:
         print(Exception)
     return rsp
-
-
-
