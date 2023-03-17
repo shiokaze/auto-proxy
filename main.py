214a235b0ef106b12e139a8bf50106c90f56aafe
shiokazexf <2240373075@qq.com>
2023-03-18 04:47:48 +0800

    update

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from uvicorn import run
 
 if __name__ == '__main__':
     print("proxy started")
-    run("pkg.server.handler:app", host="127.0.0.1", port=8071, log_level="error")
+    run("pkg.server.handler:app", host="0.0.0.0", port=8071, log_level="error")
 
